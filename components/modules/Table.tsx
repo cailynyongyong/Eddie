95ebc3ebf742ffd1dd5f4762047de7e1ac4fea98
Pedro-vd20 <pedro209.vd@gmail.com>
2021-10-27 01:48:44 -0400

    First draft student page

--- components/modules/Table.tsx
+++ components/modules/Table.tsx
@@ -6,59 +6,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-/*
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-export default function BasicTable() {
-  return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
-}
-*/
+import styles from "../../styles/Table.module.css"
 
 function createData(
     name : string,
@@ -76,28 +24,29 @@ const rows = [
 
 export default function BasicTable() {
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <TableContainer component={Paper} className={styles.noShadow}>
+      <Table className={styles.table} sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Phone Number</TableCell>
-            <TableCell align="right">Messages</TableCell>
+            <TableCell className={styles.heading}>Name</TableCell>
+            <TableCell className={styles.heading} align="right">Email</TableCell>
+            <TableCell className={styles.heading} align="right">Phone Number</TableCell>
+            <TableCell className={styles.heading} align="right">Messages</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
+              className={styles.rows}
               key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            //   sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.name}
               </TableCell>
               <TableCell align="right">{row.email}</TableCell>
               <TableCell align="right">{row.phone}</TableCell>
-              <TableCell align="right">{(row.messages ? "Subscribe" : "Unsubscribe")}</TableCell>
+              <TableCell className={(row.messages ? styles.subscribe : styles.unsubscribe)} align="right"><a href="#">{(row.messages ? "Subscribe" : "Unsubscribe")}</a></TableCell>
             </TableRow>
           ))}
         </TableBody>
--- pages/Students.tsx
+++ pages/Students.tsx
@@ -1,10 +1,18 @@
 import Homepage from "../components/modules/Homepage";
 import BasicTable from "../components/modules/Table";
+import styles from "../styles/Students.module.css";
 
 export default function Home() {
   return (
     <div>
       <Homepage />
+      <div id={styles.headers}>
+        <a href="#" className={styles.active + ' ' + styles.btn}>Students</a>
+        <a href="#" className={styles.btn}>Teaching Assistants</a>
+      </div>
+      <div className={styles.width}>
+        <p>Manage which of your students receive messages from the chatbot</p>
+      </div>
       <div id="table">
         <BasicTable />
       </div>
@@ -17,4 +25,8 @@ export default function Home() {
 Stuff left to do:
 1. Fix navbar coloring only questions
 2. Make table dynamic
+3. Make a button module with its own styles
+4. Font
+5. Refactor repetitive CSS
+6. Unify CSS with rest of stuff
 */

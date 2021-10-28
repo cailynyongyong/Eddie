b48a027d284906ac81dacf7c20ee4566219f6d7b
Pedro-vd20 <pedro209.vd@gmail.com>
2021-10-28 16:53:45 -0400

    First draft working. Next step: modularize

---
+++ pages/Test.tsx
@@ -0,0 +1,11 @@
+import Questions from "../components/modules/Questions";
+import SideBar from "../components/modules/Sidebar";
+
+export default function Home() {
+  return (
+    <div>
+      <SideBar />
+      
+    </div>
+  );
+}

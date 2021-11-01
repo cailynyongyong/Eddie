31732f9a63b03ced919cdcf0df99aba2f7f59c6a
Pedro-vd20 <pedro209.vd@gmail.com>
2021-11-01 18:14:31 -0400

    Stupid sidebar has been modularized-ish. Some work still needed

--- pages/Test.tsx
+++ pages/Test.tsx
@@ -1,11 +1,19 @@
 import Questions from "../components/modules/Questions";
 import SideBar from "../components/modules/Sidebar";
+import Box from '@mui/material/Box';
+import drawerWidth from "../components/modules/Sidebar"
 
 export default function Home() {
   return (
-    <div>
-      <SideBar />
-      
-    </div>
+     <Box sx={{ display: 'flex' }}>
+
+        <SideBar />
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+        >
+          <Questions />
+        </Box>
+    </Box>
   );
 }

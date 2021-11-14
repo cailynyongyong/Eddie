7941b2630c145ef5f152532cfa16284d03d714b9
Cailyn Yong <>
2021-11-14 14:27:10 -0500

    insights page styling

--- pages/Insights.tsx
+++ pages/Insights.tsx
@@ -1,9 +1,8 @@
-import Homepage from "../components/modules/Homepage";
+
 import SideBar from "../components/modules/Sidebar";
 import Box from '@mui/material/Box';
 import drawerWidth from "../components/modules/Sidebar";
 import IPageProps from '../interfaces/page.interface';
-import styles from "../styles/Students.module.css";
 import InsightData from "../components/modules/InsightData";
 
 const Insights: React.FunctionComponent<IPageProps> = props => {
